49d56f2eece03cffc686d73a8d26ceb1b93fcac3
DaryPet <d7akkord@gmail.com>
2024-12-12 13:29:49 +0100

    change session

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -54,7 +54,8 @@ export class AuthService {
   async login(user: any) {
     const newSession = this.createSession(user);
     const session = this.sessionRepository.create({
-      userId: user.id,
+      // userId: user.id,
+      user,
       ...newSession,
     });
 
@@ -96,7 +97,8 @@ export class AuthService {
         throw new Error('User ID is missing');
       }
       const session = this.sessionRepository.create({
-        userId: user.id,
+        // userId: user.id,
+        user,
         accessToken,
         refreshToken,
         accessTokenValidUntil,
@@ -140,9 +142,11 @@ export class AuthService {
     }
 
     // ЗАГРУЗКА ПОЛЬЗОВАТЕЛЯ ДЛЯ СОЗДАНИЯ НОВОЙ СЕССИИ
-    const user = await this.userService.findById(session.userId);
+    // const user = await this.userService.findById(session.userId);
+    const user = await this.userService.findById(session.user.id);
     if (!user) {
-      console.error('Пользователь не найден для userId:', session.userId);
+      // console.error('Пользователь не найден для userId:', session.userId);
+      console.error('Пользователь не найден для userId:', session.user.id);
       throw new Error('User not found');
     }
     if (!user.id) {
--- backend/src/auth/enteties/session.entity.ts
+++ backend/src/auth/enteties/session.entity.ts
@@ -1,4 +1,36 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+// import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+// import { User } from '../../user/entities/user.entity';
+
+// @Entity()
+// export class Session {
+//   @PrimaryGeneratedColumn()
+//   id: number;
+
+//   @Column()
+//   userId: number;
+
+//   @ManyToOne(() => User, (user) => user.sessions)
+//   user: User;
+
+//   @Column()
+//   refreshToken: string;
+
+//   @Column()
+//   accessToken: string;
+
+//   @Column()
+//   accessTokenValidUntil: Date;
+
+//   @Column()
+//   refreshTokenValidUntil: Date;
+// }
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
 import { User } from '../../user/entities/user.entity';
 
 @Entity()
@@ -6,10 +38,8 @@ export class Session {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
-  userId: number;
-
-  @ManyToOne(() => User, (user) => user.sessions)
+  @ManyToOne(() => User, (user) => user.sessions, { nullable: false })
+  @JoinColumn({ name: 'userId' }) // Указывает, что `userId` — это внешний ключ
   user: User;
 
   @Column()
