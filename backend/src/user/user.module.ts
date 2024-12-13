d1cc36c9659ac7aae8426a75789fc6667b28e4ed
DaryPet <d7akkord@gmail.com>
2024-12-13 18:45:37 +0100

    deleteuser-backend

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -3,9 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { User } from './entities/user.entity';
-
+import { Message } from '../chat/entities/message.entity'; // Импортируем сущность Message
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Message])],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService, TypeOrmModule], // Экспортируем UserService для использования в AuthModule
